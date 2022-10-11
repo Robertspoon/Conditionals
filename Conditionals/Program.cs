b2653f378bef237d7b55b411bb29951c88069763
Robertspoon <robertthemcjug@gmail.com>
2022-10-11 08:56:20 -0300

    yup

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,40 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals");
+            Console.WriteLine();
 
+            string gamertag;
+            int score;
+            score = 0;
+            gamertag = "Spoon";
+
+            if(gamertag == "Spoon")
+            {
+                score = 100000000;
+            }
+
+
+
+            Console.WriteLine("score: " + score);
+            if (score == 0)
+            {
+                Console.WriteLine("why cruel world");
+
+            }
+            else if (score > 100)
+            {
+                Console.WriteLine("I need mental help");
+            }
+
+            else
+            {
+                Console.WriteLine("I AM NOT CRAZY!");
+            }
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
